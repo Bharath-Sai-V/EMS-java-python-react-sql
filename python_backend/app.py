45a8a4cf3ffbef687d5c28cdab4cd4a34f0beaac
Bharath-Sai-V <160560250+Bharath-Sai-V@users.noreply.github.com>
2025-04-03 15:39:05 -0400

    Update app.py

--- python_backend/app.py
+++ python_backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 #Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:06J21a05b3@localhost/emspy'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password@localhost/emspy'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
